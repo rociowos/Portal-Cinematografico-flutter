27bac5170f089151ad24165a15d8205615cb4cf7
Rocio Woscoff <rociowoscoff3@gmail.com>
2024-02-12 00:48:40 -0300

    arreglos

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,52 +9,57 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bienvenido $userName a la pagina de peliculas!'),
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text('Bienvenido $userName a la pagina de peliculas!'),
+        ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            GridView.count(
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              padding: const EdgeInsets.all(20.0),
-              mainAxisSpacing: 20.0,
-              crossAxisSpacing: 20.0,
-              childAspectRatio: 1.5,
-              children: <Widget>[
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/movies');
-                  },
-                  icon: const Icon(Icons.movie),
-                  label: const Text('Peliculas'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/genres');
-                  },
-                  icon: const Icon(Icons.category),
-                  label: const Text('Generos'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/trends');
-                  },
-                  icon: const Icon(Icons.trending_up),
-                  label: const Text('Tendencias'),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/year_search');
-                  },
-                  icon: const Icon(Icons.search),
-                  label: const Text('Buscar por Año'),
-                ),
-              ],
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(20.0),
+                mainAxisSpacing: 20.0,
+                crossAxisSpacing: 20.0,
+                childAspectRatio: 1.5,
+                children: <Widget>[
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/movies');
+                    },
+                    icon: Icon(Icons.movie),
+                    label: Text('Peliculas'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/genres');
+                    },
+                    icon: Icon(Icons.category),
+                    label: Text('Generos'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/trends');
+                    },
+                    icon: Icon(Icons.trending_up),
+                    label: Text('Tendencias'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/year_search');
+                    },
+                    icon: Icon(Icons.search),
+                    label: Text('Buscar por Año'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
